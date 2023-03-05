b5d32916f5b984613b0b30a3afbd6459e7a3ea2a
laura14-03 <126986095+laura14-03@users.noreply.github.com>
2023-03-05 15:43:44 -0500

    Add files via upload

---
+++ resta1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int a,b,resta;
+  int main()
+ {
+           cout<<"ingrese el primer numero que desea restar: ";
+          cin>>a;                                           
+          cout<<"ingrese el segundo numero que desea restar : ";
+          cin>>b;                                             
+          resta=a-b;                               
+         cout<<"la resta es : "<<resta;         
+         cout<<"\n";                         
+         system("pause");
+   return 0;
+}
